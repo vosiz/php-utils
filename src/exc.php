9d0bf9c340836a5dc0f7471d4688ef5d29946a02
Petr Vosoust <petr.vosoust.ml@email.cz>
2025-05-19 01:15:41 +0200

    Extended formatted exception
    - ToString
    - inner exception

--- src/exc.php
+++ src/exc.php
@@ -1,24 +1,77 @@
 <?php
 
+require_once(__DIR__.'/interfaces/itostring.php');
+// require_once(__DIR__.'/traits/thierarchy.php');
+
 /**
  * General formatted exception
  * @param string $fmt message format ("%s" etc...)
  * @param array ...$args format arguments
  */
-class Exceptionf extends Exception {
+class Exceptionf extends Exception implements IToString {
+
+    //use THierarchy;
+
+    protected $Inner;   public function GetInner() {    return $this->Inner;}
+    protected $IsChild = false;
+
+    public static function Create(string $fmt, ...$args) {
+
+        return new Exceptionf($fmt, ...$args);
+    }
+
+    public static function CreateInner(Exceptionf $inner, $fmt, ...$args) {
+
+        $exc = self::Create($fmt, ...$args);
+        $exc->SetInner($inner);
+        return $exc;
+    }
+
+    public static function AsString($object) : string{
+
+        return Is::TypeOf($object, 'Exceptionf') ? $object->ToString() : $object->__toString();
+    }
 
     public function __construct(string $fmt, ...$args) {
 
+        // $this->Setup();
         $msg = sprintf($fmt, ...$args);
         return parent::__construct($msg);
     }
+
+    public function ToString($fmt = null) : string {
+
+        if($this->IsChild) {
+
+            return $this->getMessage().' ';
+
+        } else {
+
+            $str = '[EXCf]: '.$this->getMessage();
+            if($this->Inner !== null)
+                $str .= PHP_EOL.'- '.$this->Inner->ToString();
+            return $str;
+        }
+    }
+
+    public function SetInner(Exceptionf $exc) {  
+    
+        $this->Inner = $exc;
+        $exc->IsChild = true;
+    }
+
+    public function SetInnerExc(string $fmt, ...$args) {
+
+        return $this->SetInner(excf($fmt, ...$args));
+    }
+
 }
 
 /**
  * Application failure exception
  * @param string $msg
  */
-class ApplicationException extends Exception{
+class ApplicationException extends Exception {
 
     public function __construct(string $msg) {
 
